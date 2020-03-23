1f43d880695867308c4197fea23b45cee49dbf84
lemos <moiseslemos@gmail.com>
2020-03-23 11:09:50 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    return res.json({
+        evento: 'Semana Omnistack 11'
+    });
+})
+
+app.listen(3333);
